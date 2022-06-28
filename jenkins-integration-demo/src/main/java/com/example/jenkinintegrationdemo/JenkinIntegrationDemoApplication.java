f3b6f2933238919299bf2d6e096c7ffff351eb22
Revansidda patil <patilrevansidda@gmail.com>
2022-06-28 19:43:50 +0530

    added sample test cases

--- jenkins-integration-demo/src/main/java/com/example/jenkinintegrationdemo/JenkinIntegrationDemoApplication.java
+++ jenkins-integration-demo/src/main/java/com/example/jenkinintegrationdemo/JenkinIntegrationDemoApplication.java
@@ -1,12 +1,25 @@
 package com.example.jenkinintegrationdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class JenkinIntegrationDemoApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(JenkinIntegrationDemoApplication.class);
+
+	@PostConstruct
+	public void initt() {
+	logger.info("application started......");
+
+	}
+
 	public static void main(String[] args) {
+		logger.info("application Executed......");
 		SpringApplication.run(JenkinIntegrationDemoApplication.class, args);
 	}
 
--- jenkins-integration-demo/src/test/java/com/example/jenkinintegrationdemo/JenkinIntegrationDemoApplicationTests.java
+++ jenkins-integration-demo/src/test/java/com/example/jenkinintegrationdemo/JenkinIntegrationDemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.jenkinintegrationdemo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class JenkinIntegrationDemoApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(JenkinIntegrationDemoApplicationTests.class);
+
 	@Test
-	void contextLoads() {
+public void contextLoads() {
+		logger.info("test case is executing....");
+assertEquals(true,true);
 	}
 
 }
